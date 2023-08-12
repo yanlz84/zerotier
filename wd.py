c5b1ad3be851366f5638662b07da300a776772c3
yanlz84 <61826834+yanlz84@users.noreply.github.com>
2023-08-13 05:02:49 +0800

    Update wd.py

--- wd.py
+++ wd.py
@@ -1,14 +1,12 @@
 from webdav import WebDAVClient
 # 连接到WebDAV服务器
-def upload(url,un,pw,fp)
 client = WebDAVClient(
-    url,  # WebDAV服务器URL
-    username=un,  # 用户名
-    password=pw   # 密码
+    sys.argv[1],  # WebDAV服务器URL
+    username=sys.argv[2],  # 用户名
+    password=sys.argv[3]   # 密码
 )
 # 指定要上传的文件和目标路径
-file_path = fp  # 要上传的文件路径
+file_path = sys.argv[4]  # 要上传的文件路径
 remote_path = "/Download"  # 目标路径
 # 上传文件
 client.upload(file_path, remote_path)
-upload(url,un,pw,fp)
