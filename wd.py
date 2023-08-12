b6ca7d230344c74326b9ac2b21f8d56c3a6d7033
yanlz84 <61826834+yanlz84@users.noreply.github.com>
2023-08-13 07:25:20 +0800

    Update wd.py

--- wd.py
+++ wd.py
@@ -1,4 +1,4 @@
 import easywebdav
 import sys
-webdav = easywebdav.connect(sys.argv[1], username=sys.argv[2], password=sys.argv[3])
-webdav.upload('test', '/Download/test')
+webdav = easywebdav.connect(sys.argv[1],port=6089, username=sys.argv[2], password=sys.argv[3])
+webdav.upload('test', '/sdb1/download/test')
