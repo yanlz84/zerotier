ef1e71e0f1e9e694c42b7b63cca602b52b534032
Yan <yanlz@live.com>
2023-08-14 00:05:08 +0800

    del

--- web2.py
+++
@@ -1,8 +0,0 @@
-from webdav4.client import Client
-#import sys
-
-client = Client("http://192.168.192.11:6089", auth=("yanlz", "ylz123123"))
-print (client.exists("sdb1/download/test"))
-
-print(client.ls("sdb1/download", detail=False))
-print(client.upload_file("test.txt", "sdb1/download/test.txt"))
