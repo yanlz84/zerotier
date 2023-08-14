88ef5e673a41c5451c4c047900d1f1db77883508
Yanlz <yanlz@live.com>
2023-08-15 02:30:13 +0800

    show

--- web.py
+++ web.py
@@ -7,6 +7,7 @@ import urllib.parse
 print(sys.argv[0:])
 r=requests.get(sys.argv[4])
 if r.status_code == 200:
+    print(r.headers)
     file_name = urllib.parse.unquote(r.headers.get('Content-Disposition').split(';')[1].split('=')[1].strip('"'))
     #file_name = os.path.basename(sys.argv[4]) 
     with open(file_name, 'wb') as f:  # 打开文件，以二进制写入模式
