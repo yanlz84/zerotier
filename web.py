74f6896a181ec19b9375fe65fa730853a0e3c365
Yanlz <yanlz@live.com>
2023-08-15 02:33:10 +0800

    file_name

--- web.py
+++ web.py
@@ -4,12 +4,12 @@ import requests
 import os
 import urllib.parse
 
-print(sys.argv[0:])
+#print(sys.argv[0:])
 r=requests.get(sys.argv[4])
 if r.status_code == 200:
-    print(r.headers)
-    file_name = urllib.parse.unquote(r.headers.get('Content-Disposition').split(';')[1].split('=')[1].strip('"'))
-    #file_name = os.path.basename(sys.argv[4]) 
+    #print(r.headers)
+    #file_name = urllib.parse.unquote(r.headers.get('Content-Disposition').split(';')[1].split('=')[1].strip('"'))
+    file_name = os.path.basename(sys.argv[4]) 
     with open(file_name, 'wb') as f:  # 打开文件，以二进制写入模式
         f.write(r.content)
 client = Client(sys.argv[1], auth=(sys.argv[2], sys.argv[3]))
