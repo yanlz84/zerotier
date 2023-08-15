3e0efdace1ba21dfa0fdea77d4e4541e561c926f
Yanlz <yanlz@live.com>
2023-08-15 22:37:08 +0800

    add print

--- web.py
+++ web.py
@@ -10,12 +10,13 @@ if r.status_code == 200:
         filename = content_disposition.split('=')[1].replace('"', '')
     else:
         filename = sys.argv[4].split('/')[-1]
-
 with open(filename, 'wb') as f:
     f.write(r.content)
 
+print(filename)
 client = Client(sys.argv[1], auth=(sys.argv[2], sys.argv[3]),verify=False)
 #print (client.exists("sdb1/download/test"))
+print('upload file')
 
 #print(client.ls("", detail=False))
 print(client.upload_file(filename, "sdb1/video/download/"+filename))
