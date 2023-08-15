0940a48d9996aba7dae54ab712ffafa5a624c7f8
Yanlz <yanlz@live.com>
2023-08-15 22:13:39 +0800

    download

--- web.py
+++ web.py
@@ -1,19 +1,21 @@
 from webdav4.client import Client
 import sys
 import requests
-import os
-import urllib.parse
 
 #print(sys.argv[0:])
-r=requests.get(sys.argv[4])
+r=requests.get(sys.argv[4],allow_redirects=True)
 if r.status_code == 200:
-    #print(r.headers)
-    #file_name = urllib.parse.unquote(r.headers.get('Content-Disposition').split(';')[1].split('=')[1].strip('"'))
-    file_name = os.path.basename(sys.argv[4]) 
-    with open(file_name, 'wb') as f:  # 打开文件，以二进制写入模式
-        f.write(r.content)
-client = Client(sys.argv[1], auth=(sys.argv[2], sys.argv[3]))
+    content_disposition = r.headers.get('Content-Disposition')
+    if content_disposition:
+        filename = content_disposition.split('=')[1].replace('"', '')
+    else:
+        filename = sys.argv[4].split('/')[-1]
+
+with open(filename, 'wb') as f:
+    f.write(r.content)
+
+client = Client(sys.argv[1], auth=(sys.argv[2], sys.argv[3]),verify=False)
 #print (client.exists("sdb1/download/test"))
 
 #print(client.ls("", detail=False))
-print(client.upload_file(file_name, "sdb1/video/download/"+file_name))
+print(client.upload_file(filename, "sdb1/video/download/"+filename))
