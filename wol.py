48832df243d868e017f199668bfe67543480a04f
ylz <yanlz@live.com>
2023-12-15 14:47:40 +0800

    uppdate mac

--- wol.py
+++ wol.py
@@ -8,13 +8,13 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 your_ssh_server_ip=sys.argv[1]
 your_username=sys.argv[2]
 your_password=sys.argv[3]
-your_command=sys.argv[4]
+mac=sys.argv[4]
 
 # 连接到SSH服务器
 #client.connect(your_ssh_server_ip, username=your_username, password=your_password, port=2233)
 client.connect (your_ssh_server_ip, username=your_username, password=your_password, port=2233 )
 # 在SSH服务器上执行命令
-stdin, stdout, stderr = client.exec_command('ls')
+stdin, stdout, stderr = client.exec_command('/usr/bin/etherwake -D -i "br-lan" "'+mac+'"')
 
 # 打印命令输出
 print(stdout.read().decode('utf-8'))
